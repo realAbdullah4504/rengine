b75382ef5cf70b1a1a31c47c3f0dd01d14253ad5
realAbdullah4504 <abdullahjaved4504@gmail.com>
2025-03-24 11:04:20 +0000

    screenshot fix

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 export DEBUG=1
+
+# Run migrations
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
+
+# Start the server with debugpy
+python3 -m debugpy --listen 0.0.0.0:5678 --wait-for-client manage.py runserver 0.0.0.0:8000
 
 exec "$@"
